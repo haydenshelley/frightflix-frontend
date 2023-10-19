695bcb683ff99a7f776189b038df8c85465a96f4
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-10-19 10:31:57 -0700

    carousel for each movie category

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,22 +1,35 @@
 import { Link } from "react-router-dom";
 
 export function LandingPage(props) {
+  if (!props.movies.length) {
+    return <div>Loading...</div>;
+  }
+
   const getRandomElements = (array, count) => {
     const shuffled = array.sort(() => 0.5 - Math.random());
     return shuffled.slice(0, count);
   };
 
   const randomMovies = getRandomElements(props.movies, 3);
-  console.log(randomMovies);
+
+  const notSoScaryMovies = props.movies.filter(
+    (movie) => movie.category_id === 1
+  );
+
+  const scaryMovies = props.movies.filter((movie) => movie.category_id === 2);
+
+  const extremelyScaryMovies = props.movies.filter(
+    (movie) => movie.category_id === 3
+  );
 
   return (
     <div>
       <h1 className="creepster">Frightflix</h1>
-      <div id="carouselExampleCaptions" className="carousel slide">
+      <div id="randomMovieCarousel" className="carousel slide">
         <div className="carousel-indicators">
           <button
             type="button"
-            data-bs-target="#carouselExampleCaptions"
+            data-bs-target="#randomMovieCarousel"
             data-bs-slide-to="0"
             className="active"
             aria-current="true"
@@ -24,13 +37,13 @@ export function LandingPage(props) {
           ></button>
           <button
             type="button"
-            data-bs-target="#carouselExampleCaptions"
+            data-bs-target="#randomMovieCarousel"
             data-bs-slide-to="1"
             aria-label="Slide 2"
           ></button>
           <button
             type="button"
-            data-bs-target="#carouselExampleCaptions"
+            data-bs-target="#randomMovieCarousel"
             data-bs-slide-to="2"
             aria-label="Slide 3"
           ></button>
@@ -56,14 +69,18 @@ export function LandingPage(props) {
           </div>
           <div className="carousel-item">
             <Link to={`/movies/${randomMovies[2].id}`}>
-              <img src={randomMovies[2].image} alt="..." />
+              <img
+                src={randomMovies[2].image}
+                className="d-block w-100 carousel-image"
+                alt="..."
+              />
             </Link>
           </div>
         </div>
         <button
           className="carousel-control-prev"
           type="button"
-          data-bs-target="#carouselExampleCaptions"
+          data-bs-target="#randomMovieCarousel"
           data-bs-slide="prev"
         >
           <span
@@ -75,7 +92,7 @@ export function LandingPage(props) {
         <button
           className="carousel-control-next"
           type="button"
-          data-bs-target="#carouselExampleCaptions"
+          data-bs-target="#randomMovieCarousel"
           data-bs-slide="next"
         >
           <span
@@ -85,19 +102,179 @@ export function LandingPage(props) {
           <span className="visually-hidden">Next</span>
         </button>
       </div>
+      <p>Too scared to choose? Let me pick for you...</p>
       <div className="random-movies">
-        <p>Too scared to choose? Enter a door instead...</p>
         <Link to="/movies/random/not-so-scary">Random Not-So-Scary</Link>
         <br />
         <Link to="/movies/random/scary">Random Scary</Link>
         <br />
         <Link to="/movies/random/extremely-scary">Random Extremely Scary</Link>
       </div>
-      <Link to="/movies/not-so-scary">Not-So-Scary</Link>
-      <br />
-      <Link to="/movies/scary">Scary</Link>
-      <br />
-      <Link to="/movies/extremely-scary">Extremely Scary</Link>
+      <p>Not-So-Scary</p>
+      <div id="notSoScaryCarousel" className="carousel slide">
+        <div className="carousel-indicators">
+          {notSoScaryMovies.map((movie, index) => (
+            <button
+              key={movie.id}
+              type="button"
+              data-bs-target="#notSoScaryCarousel"
+              data-bs-slide-to={index}
+              className={index === 0 ? "active" : ""}
+              aria-label={`Slide ${index + 1}`}
+            ></button>
+          ))}
+        </div>
+        <div className="carousel-inner">
+          {notSoScaryMovies.map((movie, index) => (
+            <div
+              key={movie.id}
+              className={`carousel-item ${index === 0 ? "active" : ""}`}
+            >
+              <Link to={`/movies/${movie.id}`}>
+                <img
+                  src={movie.image}
+                  className="d-block w-100 carousel-image"
+                  alt={movie.title}
+                />
+              </Link>
+            </div>
+          ))}
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#notSoScaryCarousel"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#notSoScaryCarousel"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+      <p>Scary</p>
+      <div id="scaryMoviesCarousel" className="carousel slide">
+        <div className="carousel-indicators">
+          {scaryMovies.map((movie, index) => (
+            <button
+              key={movie.id}
+              type="button"
+              data-bs-target="#scaryMoviesCarousel"
+              data-bs-slide-to={index}
+              className={index === 0 ? "active" : ""}
+              aria-label={`Slide ${index + 1}`}
+            ></button>
+          ))}
+        </div>
+        <div className="carousel-inner">
+          {scaryMovies.map((movie, index) => (
+            <div
+              key={movie.id}
+              className={`carousel-item ${index === 0 ? "active" : ""}`}
+            >
+              <Link to={`/movies/${movie.id}`}>
+                <img
+                  src={movie.image}
+                  className="d-block w-100 carousel-image"
+                  alt={movie.title}
+                />
+              </Link>
+            </div>
+          ))}
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#scaryMoviesCarousel"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#scaryMoviesCarousel"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+      <p>Extremely Scary</p>
+      <div id="extremelyScaryMoviesCarousel" className="carousel slide">
+        <div className="carousel-indicators">
+          {extremelyScaryMovies.map((movie, index) => (
+            <button
+              key={movie.id}
+              type="button"
+              data-bs-target="#extremelyScaryMoviesCarousel"
+              data-bs-slide-to={index}
+              className={index === 0 ? "active" : ""}
+              aria-label={`Slide ${index + 1}`}
+            ></button>
+          ))}
+        </div>
+        <div className="carousel-inner">
+          {extremelyScaryMovies.map((movie, index) => (
+            <div
+              key={movie.id}
+              className={`carousel-item ${index === 0 ? "active" : ""}`}
+            >
+              <Link to={`/movies/${movie.id}`}>
+                <img
+                  src={movie.image}
+                  className="d-block w-100 carousel-image"
+                  alt={movie.title}
+                />
+              </Link>
+            </div>
+          ))}
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#extremelyScaryMoviesCarousel"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#extremelyScaryMoviesCarousel"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
     </div>
   );
 }
